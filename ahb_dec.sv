c0253bf46860be649d772b315e8e67c1737cfdd4
MPOviyaNandini <sct.mpo.0320@gmail.com>
2025-08-01 10:49:36 +0530

    Updated ahb_dec.sv

--- ahb_dec.sv
+++ ahb_dec.sv
@@ -1,5 +1,4 @@
-
-module ahb_dec (
+/*module ahb_dec (
     input  logic [31:0] haddr,
     output logic        HSEL1,
     output logic        HSEL2,
@@ -27,5 +26,31 @@ always_comb begin
         end
     endcase
 end
+
+endmodule*/
+
+
+module ahb_dec (
+    input  logic [31:0] haddr,
+    output logic        HSEL1,
+    output logic        HSEL2,
+    output logic        muxsel
+);
+
+always_comb begin
+    HSEL1  = 1'b0;
+    HSEL2  = 1'b0;
+    muxsel = 1'b0;
+    if(haddr[31:24]== 8'hA0)begin
+            HSEL1  = 1'b1;
+            muxsel = 1'b1;   
+    end
+    else begin
+            HSEL2  = 1'b1;
+            muxsel = 1'b0;    
+    end
+    
+end
+
 
 endmodule
